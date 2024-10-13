4051c6467d2af84e644ed537ec2cc6c2b9db55ad
Philippe DUBOIS <philippedub91.av@gmail.com>
2024-10-13 16:22:41 +0200

    setup
    Initialisation du serial et du wifi

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,30 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <ArduinoJson.h>
-#include <config.h>
+#include <WiFiManager.h>
 
 
 void setup() {
-  // Not implemented yet
+  // Initialisation du port série
+  Serial.begin(9600);
+  delay(1000);
+  Serial.println("Port série OK");
+
+  // Initialisation du WiFI
+  WiFiManager wifiManager;
+  if(!wifiManager.autoConnect("rhapsody_relays_AP")) {
+    Serial.println("Echec de la connexion ou de la création du point d'accès");
+    ESP.reset();
+    delay(1000);
+  }
+  Serial.println("Connecté au WiFi !");
+  Serial.print("Adresse IP : ");
+  Serial.println(WiFi.localIP());
+
+  
 }
 
 
 void loop() {
-  // Not implemented yet
+  Serial.println("Bonjour tout le monde !");
 }
