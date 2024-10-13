2adc661e81d7aaab9292d8bb2ac26fab11ecd57a
Philippe DUBOIS <philippedub91.av@gmail.com>
2024-10-13 15:20:10 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ArduinoJson.h>
+#include <config.h>
+
+
+void setup() {
+  // Not implemented yet
+}
+
+
+void loop() {
+  // Not implemented yet
+}
